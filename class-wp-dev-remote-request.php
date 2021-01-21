fbe6af128e527f456419dd90d90afb6ff8cec31a
Mahesh Waghmare <mwaghmare7@gmail.com>
2021-01-21 19:53:24 +0530

    Release v1.0.0

--- class-wp-dev-remote-request.php
+++ class-wp-dev-remote-request.php
@@ -1,29 +1,28 @@
 <?php
 /**
- * WP Dev Remote Request
+ * Plugin Name: WP Dev Remote Request
+ * Description: Cache the HTTP reqeust and store into the transient for given expiration time to avoid the remote requests.
+ * Plugin URI: https://profiles.wordpress.org/mahesh901122/
+ * Author: Mahesh M. Waghmare
+ * Author URI: https://maheshwaghmare.com/
+ * Version: 1.0.0
+ * License: GNU General Public License v2.0
+ * Text Domain: wp-dev-remote-request
  *
- * How to use?
- * 
- * Example 1: Simple.
- * 
- * $response = wp_dev_remote_request_get( 'http://example.com/wp-json/wp/v2/posts/' ); // Required.
+ * # How to use?
  *
- * OR
+ * ## Example 1)
  *
- * Example 2: Advanced.
- * 
+ * $response = wp_dev_remote_request_get( 'https://maheshwaghmare.com/wp-json/wp/v2/posts/' );
+ *
+ * ## Example 2: Advanced.
+ *
  * $response = wp_dev_remote_request_get( array(
- * 					'url' => '', // Required. Rest API URL. Default empty.
- * 					'request_args' => array(), // Optional. Rest API arguments list. E.g. timeout etc.  Default array( 'timeout' => 60 )
- * 					'force' => true, // Optional. Avoid `expiration` check and forcefully trigger the request. Default false. 
- * 					'expiration' => 30, // Optional. Expiry time of transient. Default MONTH_IN_SECONDS.
- *					'generate_json_file' => array(  // Optional. File Generation. Default empty.
- *						'file_name' => '', // Required. File name.
- *						'option_name' => '', // Required. Option name to save the values.
- *						'location' => '', // Required. File location.
- *						'generate_file_if' => true, // Required. Generate though condition.
- *					)
- *				) );
+ *                  'url' => 'https://maheshwaghmare.com/wp-json/wp/v2/posts/',
+ *                  'query_args' => array(
+ *                      'per_page' => 5,
+ *                  )
+ *              );
  *
  * @package WP Dev Remote Request
  * @since 1.0.0
@@ -46,18 +45,27 @@ if ( ! class_exists( 'WP_Dev_Remote_Request' ) ) :
 		 * @since 1.0.0
 		 */
 		private static $instance;
-		private $max_request_limit = 3;
 
-		private $default_request_args = array(
-			'timeout' => 60,
-		);
+		/**
+		 * Arguments
+		 *
+		 * @access private
+		 * @var object Class object.
+		 * @since 1.0.0
+		 */
+		private $args = array(
+			'url'         => '',
+			'query_args'  => array(),
+			'remote_args' => array(
+				'timeout' => 60,
+			),
+			'expiration'  => MONTH_IN_SECONDS,
+			'force'       => false,
 
-		private $default_args = array(
-			'url' => '',
-			'args' => array(),
-			'expiration' => MONTH_IN_SECONDS,
-			'force' => false, // Avoid `expiration` check and forcefully trigger the request.
-			'generate_json_file' => false, // Avoid `expiration` check and forcefully trigger the request.
+			// Debugging.
+			'start_time'  => '',
+			'end_time'    => '',
+			'duration'    => '',
 		);
 
 		/**
@@ -78,105 +86,130 @@ if ( ! class_exists( 'WP_Dev_Remote_Request' ) ) :
 		 *
 		 * @since 1.0.0
 		 */
-		public function __construct() {
-		}
+		public function __construct() {}
 
 		/**
 		 * Remote GET API Request
 		 *
-		 * @since x.x.x
+		 * @since 1.0.0
 		 *
-		 * @param  mixed  $args    Request URL/Array of arguments for the API request.
+		 * @param  mixed $args    Request URL/Array of arguments for the API request.
 		 * @return mixed            Return the API request result.
 		 */
 		public function remote_get( $args = array() ) {
 
-			if( empty( $args ) ) {
-				return new WP_Error( __( 'Invalid Arguments.', 'wp-dev-remote-request' ) );
+			$this->args['start_time'] = time();
+
+			// Validate argumenents.
+			if ( empty( $args ) ) {
+				return array(
+					'success' => false,
+					'message' => __( 'Invalid Paraneters.', 'wp-dev-remote-request' ),
+				);
 			}
 
-			$request_endpoint = '';
-			$request_args = $this->default_request_args;
-			$force = false;
-			$expiration = MONTH_IN_SECONDS;
-
-			// If $args contain only API url then set the default arguments.
-			if( is_string( $args ) && ! is_array( $args ) ) {
-				$request_endpoint = sanitize_text_field( $args );
-				$request_args = $this->default_request_args;
-			} else {
-				if( isset( $args['url'] ) ) {
-					$request_endpoint = sanitize_text_field( $args['url'] );
-				}
-				if( isset( $args['args'] ) ) {
-					$request_args = wp_parse_args( $args['args'], $this->default_request_args );
-				}
-				if( isset( $args['transient_key'] ) ) {
-					$transient_key = sanitize_key( $args['transient_key'] );
-				}
-				if( isset( $args['expiration'] ) ) {
-					$expiration = sanitize_key( $args['expiration'] );
-				}
-				if( isset( $args['force'] ) ) {
-					$force = $args['force'];
-				}
-			}
+			// Get request rguments.
+			$this->set_args( $args );
 
 			// If empty then return.
-			if( empty( $request_endpoint ) ) {
-				return new WP_Error( __( 'Invalid Request Endpoint', 'wp-dev-remote-request' ) );
+			if ( empty( $this->args['url'] ) ) {
+				return array(
+					'success' => false,
+					'message' => __( 'Invalid Request Endpoint.', 'wp-dev-remote-request' ),
+				);
 			}
 
-			$unique_request_key = md5( $request_endpoint . json_encode( $request_args ) );
-			$transient_key = 'wp-dev-remote-request-' . $unique_request_key;
-
-			// Request URL.
-			// $request_url = trailingslashit( $this->get_domain() ) . $request_endpoint;
-			$request_url = $request_endpoint;
+			$this->log( 'REQUEST URL: ' . $this->args['url'] );
+			$this->log( 'ARGS: ' . wp_json_encode( $this->args ) );
+			$this->log( 'TRANSIENT_KEY: ' . $this->get_transient_key() );
 
 			/**
-			 * If `force` is not set then check request maximum requests count. If it reach the maximum requests the return transient output.
+			 * If `force` is not set then check request maximum requests count.
+			 * If it reach the maximum requests the return transient output.
 			 */
-			if( false === $force ) {
-				// Check in transient and return its cached transient data.			
-				$transient_flag = get_transient( $transient_key );
+			$cached_data = get_transient( $this->get_transient_key() );
+			if ( false === $this->args['force'] && false !== $cached_data ) {
 
-				// Check Max Request Limit.
-				// Avoid multiple requests and serve data from the transient.
-				$request_limit_key = 'wp-dev-remote-request-request-limit-' . $unique_request_key;
-				$request_limit = (int) get_transient( $request_limit_key );
-				if( $request_limit >= $this->max_request_limit ) {
-					return array(
-						'success' => true,
-						'message' => __( 'Reached MAX remote requests. Response from transient.', 'wp-dev-remote-request' ),
-						'data'    => $transient_flag,
-						'expiration'  => $expiration,
-					);
+				$this->log( 'RESULT: (Cached) ' . wp_json_encode( $cached_data ) );
+				$this->log( 'MESSAGE: ' . __( 'Response from transient.', 'wp-dev-remote-request' ) );
+
+				// Duration.
+				$this->args['end_time'] = time();
+				$this->args['duration'] = $this->args['end_time'] - $this->args['start_time'];
+				$this->log( 'DURATION: ' . human_time_diff( $this->args['end_time'], $this->args['start_time'] ) );
+				// Duration.
+
+				return array(
+					'success'    => true,
+					'message'    => __( 'Response from transient.', 'wp-dev-remote-request' ),
+					'data'       => $cached_data,
+					'expiration' => $this->args['expiration'],
+				);
+
+			} else {
+
+				$request = wp_remote_get( $this->args['url'], $this->args['remote_args'] );
+				$result  = $this->request( $request );
+
+				$this->log( 'RESULT: (Live) ' . wp_json_encode( $result['data'] ) );
+
+				if ( $result['success'] ) {
+					set_transient( $this->get_transient_key(), $result['data'], $this->args['expiration'] );
 				}
-				set_transient( $request_limit_key, ($request_limit + 1), $expiration );
 
-				// Serve response from the transient if transient data is not empty.
-				if( false !== $transient_flag ) {
-					return array(
-						'success' => true,
-						'message' => __( 'Response from transient.', 'wp-dev-remote-request' ),
-						'data'    => $transient_flag,
-						'expiration'  => $expiration,
-					);
+				$this->log( 'MESSAGE: ' . $result['message'] );
+
+				// Duration.
+				$this->args['end_time'] = time();
+				$this->args['duration'] = $this->args['end_time'] - $this->args['start_time'];
+				$this->log( 'DURATION: ' . human_time_diff( $this->args['end_time'], $this->args['start_time'] ) );
+				// Duration.
+
+				return $result;
+			}
+		}
+
+		/**
+		 * Get uniqueue request key
+		 */
+		public function get_unique_request_key() {
+			return md5( $this->args['url'] . wp_json_encode( $this->args['query_args'] ) );
+		}
+
+		/**
+		 * Get transient key
+		 */
+		public function get_transient_key() {
+			return 'wp-dev-remote-request-' . $this->get_unique_request_key();
+		}
+
+		/**
+		 * Set Arguments
+		 *
+		 * @param array $args   Request arguments.
+		 */
+		public function set_args( $args = array() ) {
+
+			if ( is_string( $args ) && ! is_array( $args ) ) {
+				$args = array(
+					'url' => $args,
+				);
+			} else {
+
+				// Merge remote args with default remote args.
+				if ( isset( $args['remote_args'] ) ) {
+					$args['remote_args'] = wp_parse_args( $args['remote_args'], $this->args['remote_args'] );
+				}
+
+				// Merge query args with default query args.
+				if ( isset( $args['query_args'] ) ) {
+					$query_args         = wp_parse_args( $args['query_args'], $this->args['query_args'] );
+					$args['query_args'] = $query_args;
+					$args['url']        = add_query_arg( $query_args, trailingslashit( $args['url'] ) );
 				}
 			}
 
-			$request = wp_remote_get( $request_url, $request_args );
-			$result = $this->request( $request, $args );
-
-			if( $result['success'] ) {
-				set_transient( $transient_key, $result['data'], $expiration );
-
-				// file generation.
-				$this->generate_file( $args, $result );
-			}
-
-			return $result;
+			$this->args = wp_parse_args( $args, $this->args );
 		}
 
 		/**
@@ -189,23 +222,23 @@ if ( ! class_exists( 'WP_Dev_Remote_Request' ) ) :
 		 * @param  array $request    Array of arguments for the API request.
 		 * @return mixed           Return the API request result.
 		 */
-		public function request( $request, $args = array() ) {
+		public function request( $request ) {
 
 			// Is WP Error?
 			if ( is_wp_error( $request ) ) {
 				return array(
 					'success' => false,
 					'message' => $request->get_error_message(),
-					'data'    => wp_remote_retrieve_body($request),
+					'data'    => wp_remote_retrieve_body( $request ),
 				);
 			}
 
 			// Invalid response code.
-			if ( wp_remote_retrieve_response_code( $request ) != 200 ) {
+			if ( wp_remote_retrieve_response_code( $request ) !== 200 ) {
 				return array(
 					'success' => false,
 					'message' => $request['response'],
-					'data'    => wp_remote_retrieve_body($request),
+					'data'    => wp_remote_retrieve_body( $request ),
 				);
 			}
 
@@ -231,66 +264,15 @@ if ( ! class_exists( 'WP_Dev_Remote_Request' ) ) :
 			);
 		}
 
-		public function generate_file( $args = array(), $response = array() ) {
-			if( empty( $args ) ) {
-				return;
-			}
-
-			$file_generate_args = isset( $args['generate_json_file'] ) ? array_map('esc_attr', $args['generate_json_file']) : array();
-
-			// Not have file generation arguments.
-			if( empty( $file_generate_args ) ) {
-				return;
-			}
-
-			$file_name = isset( $file_generate_args['file_name'] ) ? sanitize_file_name( $file_generate_args['file_name'] ) : '';
-			$option_name = isset( $file_generate_args['file_name'] ) ? sanitize_text_field( $file_generate_args['option_name'] ) : '';
-			$location = isset( $file_generate_args['location'] ) ? trailingslashit( $file_generate_args['location'] ) : '';
-			$generate_file_if = isset( $file_generate_args['generate_file_if'] ) ? $file_generate_args['generate_file_if'] : false;
-
-			// Return if file generation flag is false.
-			if( false === $generate_file_if ) {
-				return;
-			}
-
-			// File name/location is empty then return.
-			if( empty( $file_name ) || empty( $location ) ) {
-				return;
-			}
-
-			// If response is empty then return.
-			if( empty( $response ) ) {
-				return;
-			}
-
-			// Remove unwanted data to save in JSON file.
-			unset( $response['success'] );
-			unset( $response['message'] );
-
-			// Save in option table if option name is provided.
-			if( ! empty( $option_name ) ) {
-				update_option( $option_name, $response );
-				
-				$response['option_name'] = $option_name;
-			}
-
-			$this->get_filesystem()->put_contents( $location . $file_name . '.json', wp_json_encode( $response ) );
-		}
-
 		/**
-		 * Get an instance of WP_Filesystem_Direct.
+		 * Log
 		 *
-		 * @since 2.0.0
-		 * @return object A WP_Filesystem_Direct instance.
+		 * @param string $message   Log message.
 		 */
-		public function get_filesystem() {
-			global $wp_filesystem;
-
-			require_once ABSPATH . '/wp-admin/includes/file.php';
-
-			WP_Filesystem();
-
-			return $wp_filesystem;
+		public function log( $message = '' ) {
+			if ( defined( 'WP_DEBUG' ) && WP_DEBUG ) {
+				error_log( $message ); // phpcs:ignore WordPress.PHP.DevelopmentFunctions.error_log_error_log
+			}
 		}
 
 	}
@@ -300,7 +282,16 @@ if ( ! class_exists( 'WP_Dev_Remote_Request' ) ) :
 
 endif;
 
-if( ! function_exists( 'wp_dev_remote_request_get' ) ) :
+if ( ! function_exists( 'wp_dev_remote_request_get' ) ) :
+
+	/**
+	 * Remote GET API Request
+	 *
+	 * @since 1.0.0
+	 *
+	 * @param  mixed $args    Request URL/Array of arguments for the API request.
+	 * @return mixed            Return the API request result.
+	 */
 	function wp_dev_remote_request_get( $args = array() ) {
 		return WP_Dev_Remote_Request::get_instance()->remote_get( $args );
 	}
